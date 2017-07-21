855f57faebec3011902f6c3ce40498b1d571fa0a
Nicolás Patarino <npatarino@idealista.com>
2017-07-21 13:08:33 +0200

    First approach filtering res/drawable resources not used in res/layout

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,21 @@
+import java.io.File
+
+val ROOT_PROJECT_PATH = "/home/argento/projects/idealista/android-studio-project/application/"
+
+val RES_DRAWABLE_PATH = "app/src/main/res/drawable"
+val RES_LAYOUT_PATH = "app/src/main/res/layout"
 
 fun main(args: Array<String>) {
-    println("Hola Fran")
+
+    File(ROOT_PROJECT_PATH, RES_DRAWABLE_PATH)
+            .listFiles().map { it.nameWithoutExtension }.filter {
+        name ->
+        File(ROOT_PROJECT_PATH, RES_LAYOUT_PATH).listFiles().filter {
+            file ->
+            file.readText().contains(name)
+        }.isEmpty()
+    }.map {
+        println(it)
+    }
+
 }
