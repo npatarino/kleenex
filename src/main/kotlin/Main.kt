2e48ff49c18e5f982fc207e2ceddf42040950326
Victor de Francisco <vfrancisco@idealista.com>
2017-07-26 13:23:54 +0200

    Include function to find unused resources from different folders

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,44 @@
 import java.io.File
 import java.util.*
 
-val ROOT_PROJECT_PATH = "/home/argento/projects/idealista/"
+val ROOT_PROJECT_PATH = "/home/vfrancisco/develop/idealista-android"
 
 val RES_DRAWABLE_PATH = "app/src/main/res/drawable"
+val RES_HDPI_DRAWABLE_PATH = RES_DRAWABLE_PATH.plus("-hdpi")
 val APP_PATH = "app/src/main"
 
 fun main(args: Array<String>) {
     val startTime = Date().time
-    searchNonUsedDrawables(File(ROOT_PROJECT_PATH, APP_PATH), File(ROOT_PROJECT_PATH, RES_DRAWABLE_PATH)).map {
+
+    val folders = listOf(fileFromRelativePath(RES_DRAWABLE_PATH), fileFromRelativePath(RES_HDPI_DRAWABLE_PATH))
+    searchNonUsedResourcesFromFolders(folders).map {
         showToConsole(it)
     }
+
     val endTime = Date().time
     println((endTime - startTime) / 1000)
 }
 
-private fun searchNonUsedDrawables(filesRoot: File, drawablesRoot: File) =
-        filesRoot.walkTopDown()
+private fun searchNonUsedResourcesFromFolders(folders: List<File>) : List<File> =
+    folders.map {
+        searchNonUsedResourcesFromFolder(fileFromRelativePath(APP_PATH), it)
+    }.flatten()
+
+private fun searchNonUsedResourcesFromFolder(folder: File, resourcesFolder: File) : List<File> =
+        folder.walkTopDown()
                 .filter { isFile(it) }
                 .asIterable()
                 .map {
-                    getNonUsedDrawablesWithNull(it, drawablesRoot)
+                    getNonUsedDrawablesWithNull(it, resourcesFolder)
                 }.flatten()
                 .filterNotNull()
                 .sortedBy {
                     byName(it)
                 }.distinct()
 
-private fun showToConsole(it: File) {
-    println(it.nameWithoutExtension)
-}
+private fun showToConsole(it: File) = println(it.nameWithoutExtension)
 
-private fun byName(it: File) = it.nameWithoutExtension
+private fun byName(it: File) : String = it.nameWithoutExtension
 
 private fun getNonUsedDrawablesWithNull(lf: File, drawablesRoot: File): List<File?> {
     val text = lf.readText()
@@ -41,6 +48,10 @@ private fun getNonUsedDrawablesWithNull(lf: File, drawablesRoot: File): List<Fil
     }
 }
 
-private fun isFile(it: File) = it.isFile
+private fun isFile(it: File) : Boolean = it.isFile
 
-private fun containsFilename(text: String, df: File) = text.contains(df.nameWithoutExtension)
+private fun containsFilename(text: String, df: File) : Boolean =
+    text.contains(Regex("^${df.nameWithoutExtension}$"))
+
+private fun fileFromRelativePath(relativePath : String) : File =
+        File(ROOT_PROJECT_PATH, relativePath)
