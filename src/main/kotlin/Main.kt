b3a67427747923e8f96b6da795e168c925863a2f
Nicolás Patarino <npatarino@gmail.com>
2017-07-26 12:19:45 +0200

    Extract a method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,50 +1,32 @@
 import java.io.File
 import java.util.*
 
-val ROOT_PROJECT_PATH = "/home/argento/projects/idealista/android-studio-project/application/"
+val ROOT_PROJECT_PATH = "/home/argento/projects/idealista/"
 
 val RES_DRAWABLE_PATH = "app/src/main/res/drawable"
 val APP_PATH = "app/src/main"
 
 fun main(args: Array<String>) {
-
     val startTime = Date().time
-
-    searchNonUsedDrawables(File(ROOT_PROJECT_PATH, APP_PATH), File(ROOT_PROJECT_PATH, RES_DRAWABLE_PATH))
-
+    searchNonUsedDrawables(File(ROOT_PROJECT_PATH, APP_PATH), File(ROOT_PROJECT_PATH, RES_DRAWABLE_PATH)).map {
+        showToConsole(it)
+    }
     val endTime = Date().time
-
     println((endTime - startTime) / 1000)
-
-    /*File(ROOT_PROJECT_PATH, RES_DRAWABLE_PATH)
-            .listFiles().filter {
-        drawableFile ->
-        File(ROOT_PROJECT_PATH, APP_PATH).listFiles().filter {
-            file ->
-            file.readText().contains(drawableFile.nameWithoutExtension)
-        }.isEmpty()
-    }.map {
-        println(it.nameWithoutExtension)
-    }*/
-
 }
 
 
-private fun searchNonUsedDrawables(filesRoot: File, drawablesRoot: File) {
-    filesRoot.walkTopDown()
-            .filter { isFile(it) }
-            .asIterable()
-            .map {
-                getNonUsedDrawablesWithNull(it, drawablesRoot)
-            }.flatten()
-            .filterNotNull()
-            .sortedBy {
-                byName(it)
-            }.distinct()
-            .map {
-                showToConsole(it)
-            }
-}
+private fun searchNonUsedDrawables(filesRoot: File, drawablesRoot: File) =
+        filesRoot.walkTopDown()
+                .filter { isFile(it) }
+                .asIterable()
+                .map {
+                    getNonUsedDrawablesWithNull(it, drawablesRoot)
+                }.flatten()
+                .filterNotNull()
+                .sortedBy {
+                    byName(it)
+                }.distinct()
 
 private fun showToConsole(it: File) {
     println(it.nameWithoutExtension)
